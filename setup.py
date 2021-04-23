db56773c70b1808ac9a26df2aa5e4c414d789deb
Grigoriy Shoroshov <shoroh2525@gmail.com>
2021-04-23 17:37:48 +0300

    Fix flake8 errors in setup.py and tests

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ from distutils.core import setup
 setup(
     name='dl_markup',
     version='0.0.1',
-    packages=['dl_markup',],
+    packages=['dl_markup', ],
     long_description=open('README.md').read(),
     entry_points={
         'console_scripts': [
--- tests/empty_test.py
+++ tests/empty_test.py
@@ -1,5 +1,2 @@
-import pytest
-
-
 def test_always_pass():
     assert True
