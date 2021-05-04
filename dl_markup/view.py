b7b9fcef747f63340857440f295b9e336e1f62c9
Dmitry Lyukov <d@lyukov.com>
2021-05-05 02:08:40 +0300

    Add imports for all widgets in View

--- dl_markup/view.py
+++ dl_markup/view.py
@@ -1,41 +1,49 @@
-from PyQt5 import QtWidgets
+from PyQt5.QtWidgets import QLabel
+from PyQt5.QtWidgets import QListView
+from PyQt5.QtWidgets import QVBoxLayout
+from PyQt5.QtWidgets import QHBoxLayout
+from PyQt5.QtWidgets import QWidget
+from PyQt5.QtWidgets import QGraphicsView
+from PyQt5.QtWidgets import QToolBar
+from PyQt5.QtWidgets import QFileDialog
+from PyQt5.QtWidgets import QMainWindow
 
 
-class View(QtWidgets.QMainWindow):
+class View(QMainWindow):
     """
     Application view
     """
     def __init__(self, scene):
         super().__init__()
         self.scene = scene
-        self.fileList = QtWidgets.QListView()
+        self.fileList = QListView()
         self.setWindowTitle("DL Markup")
-        self.inputDirectory = QtWidgets.QLabel('.')
-        self.outputDirectory = QtWidgets.QLabel('.')
+        self.inputDirectory = QLabel('.')
+        self.outputDirectory = QLabel('.')
 
-        mainLayout = QtWidgets.QVBoxLayout()
-        centralWidget = QtWidgets.QWidget(self)
+        mainLayout = QVBoxLayout()
+        centralWidget = QWidget(self)
         self.setCentralWidget(centralWidget)
         centralWidget.setLayout(mainLayout)
         mainLayout.addWidget(self.inputDirectory)
         mainLayout.addWidget(self.outputDirectory)
 
-        layout = QtWidgets.QHBoxLayout()
+        layout = QHBoxLayout()
         mainLayout.addLayout(layout)
         layout.addWidget(self.fileList)
-        layout.addWidget(QtWidgets.QGraphicsView(scene))
+        layout.addWidget(QGraphicsView(scene))
 
         self._createToolbar()
 
     def _createToolbar(self):
-        tools = QtWidgets.QToolBar()
+        tools = QToolBar()
         self.addToolBar(tools)
         tools.addAction('Select input directory', self._selectInputDirectory)
         tools.addAction('Select output directory', self._selectOutputDirectory)
         tools.addAction('Save', print)
 
     def _selectInputDirectory(self):
-        self.inputDirectory.setText(QtWidgets.QFileDialog.getExistingDirectory())
+        self.inputDirectory.setText(QFileDialog.getExistingDirectory())
 
     def _selectOutputDirectory(self):
-        self.outputDirectory.setText(QtWidgets.QFileDialog.getExistingDirectory())
+        self.outputDirectory.setText(QFileDialog.getExistingDirectory())
