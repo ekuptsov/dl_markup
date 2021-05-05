1f8f4057edfc4310ef18459bba3f9d44bfd124fa
Dmitry Lyukov <d@lyukov.com>
2021-05-05 19:39:19 +0300

    Update file list on changing input directory

--- dl_markup/view.py
+++ dl_markup/view.py
@@ -7,6 +7,9 @@ from PyQt5.QtWidgets import QGraphicsView
 from PyQt5.QtWidgets import QToolBar
 from PyQt5.QtWidgets import QFileDialog
 from PyQt5.QtWidgets import QMainWindow
+import os
+
+from .list_model import ListModel
 
 
 class View(QMainWindow):
@@ -16,7 +19,9 @@ class View(QMainWindow):
     def __init__(self, scene):
         super().__init__()
         self.scene = scene
+        self.listModel = ListModel()
         self.fileList = QListView()
+        self.fileList.setModel(self.listModel)
         self.setWindowTitle("DL Markup")
         self.inputDirectory = QLabel('.')
         self.outputDirectory = QLabel('.')
@@ -45,6 +50,7 @@ class View(QMainWindow):
 
     def _selectInputDirectory(self):
         self.inputDirectory.setText(QFileDialog.getExistingDirectory())
+        self._updateFileList()
 
     def _selectOutputDirectory(self):
         self.outputDirectory.setText(QFileDialog.getExistingDirectory())
@@ -54,3 +60,8 @@ class View(QMainWindow):
 
     def _save(self):
         pass
+
+    def _updateFileList(self):
+        files = os.listdir(self.inputDirectory.text())
+        print("Updating file list:", files)
+        self.listModel.setItems(files)
