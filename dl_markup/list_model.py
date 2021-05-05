b439be96dc5505db9d2f8538d9e3b4e4a39d8111
Dmitry Lyukov <d@lyukov.com>
2021-05-05 19:38:54 +0300

    Add ListModel class

---
+++ dl_markup/list_model.py
@@ -0,0 +1,18 @@
+from PyQt5 import QtCore
+from PyQt5.QtCore import Qt
+
+class ListModel(QtCore.QAbstractListModel):
+    def __init__(self, *args, items=None, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.items = items or []
+
+    def data(self, index, role):
+        if role == Qt.DisplayRole:
+            return self.items[index.row()]
+
+    def rowCount(self, index):
+        return len(self.items)
+
+    def setItems(self, items):
+        self.items = items
+        self.layoutChanged.emit()
