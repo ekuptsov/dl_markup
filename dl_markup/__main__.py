36c585be0a78d80f66165817ca59934f9b0d229e
Dmitry Lyukov <d@lyukov.com>
2021-05-05 21:46:48 +0300

    Remove initial drawing

--- dl_markup/__main__.py
+++ dl_markup/__main__.py
@@ -10,23 +10,11 @@ from .canvas import Canvas
 
 def main():
     app = QtWidgets.QApplication([])
-
     scene = Scene(0, 0, 512, 512)
     scene.img = QtGui.QPixmap('resources/Lenna.png')
     undo_redo = UndoRedo(scene)
-
-    cylinder = CylinderItem(
-        QtCore.QPointF(100, 100),
-        QtCore.QPointF(200, 200),
-        20,
-        pen=QtGui.QPen(QtGui.QColor(0, 255, 0)),
-        brush=QtGui.QBrush(QtGui.QColor(0, 255, 0))
-    )
-    undo_redo.insert_in_undo_redo_add(cylinder)
-
     canvas = Canvas(scene, undo_redo)
     model = Model(scene)
     view = View(model, canvas)
     view.show()
-
     app.exec_()
